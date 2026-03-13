4c2422b0d5b95a0c56e113bd45ceaf3c65c4a610
c-jj55 <125046742+c-jj55@users.noreply.github.com>
2026-03-13 19:39:10 +0800

    Remove ua3f repository address from script
    Remove outdated repository address for ua3f.

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,8 +10,7 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-# 替换为兼容性更好的仓库地址
-echo 'src-git ua3f https://github.com/mizhen9/luci-app-ua3f.git' >> feeds.conf.default
+
 
 # Lucky 保持现状
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
