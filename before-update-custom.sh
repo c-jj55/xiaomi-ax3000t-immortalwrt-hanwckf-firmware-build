9953c548e0de1e401b61f45b97b3a34f515f1e13
c-jj55 <125046742+c-jj55@users.noreply.github.com>
2026-03-13 18:11:32 +0800

    Update UA3F repository source in script

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,6 +10,8 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-# 使用更稳定的镜像源，绕过身份验证问题
-echo 'src-git ua3f https://github.com/immortalwrt-collections/openwrt-ua3f.git' >> feeds.conf.default
+# 真正的 UA3F 官方源码仓库
+echo 'src-git ua3f https://github.com/SunBK201/UA3F.git' >> feeds.conf.default
+
+# Lucky 的地址没问题，继续保留
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
