e0d9fbd0324a3c110747ccf370b997358d01937f
c-jj55 <125046742+c-jj55@users.noreply.github.com>
2026-03-13 19:29:54 +0800

    Update repository URLs for compatibility

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,8 +10,8 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-# 真正的 UA3F 官方源码仓库
-echo 'src-git ua3f https://github.com/SunBK201/UA3F.git' >> feeds.conf.default
+# 替换为兼容性更好的仓库地址
+echo 'src-git ua3f https://github.com/mizhen9/luci-app-ua3f.git' >> feeds.conf.default
 
-# Lucky 的地址没问题，继续保留
+# Lucky 保持现状
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
