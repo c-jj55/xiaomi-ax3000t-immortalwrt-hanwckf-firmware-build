f18da65aef4b571757b0b84136220297016d8979
c-jj55 <125046742+c-jj55@users.noreply.github.com>
2026-03-13 18:00:41 +0800

    Update repository sources for ua3f and lucky

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,8 +10,6 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-# 添加 ua3f 源码仓库
-echo 'src-git ua3f https://github.com/chenhw2/luci-app-ua3f.git' >> feeds.conf.default
-
-# 添加 Lucky 源码仓库 (以防官方库没收录)
+# 使用更稳定的镜像源，绕过身份验证问题
+echo 'src-git ua3f https://github.com/immortalwrt-collections/openwrt-ua3f.git' >> feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
